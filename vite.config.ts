90c7ca025bcabdd9abd5d9a6608512f063fdd912
Siedel <74981165+Saideru@users.noreply.github.com>
2026-08-21 17:43:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    publicDir: 'public', 
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
